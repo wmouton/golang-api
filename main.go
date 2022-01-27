64a6c4a90b0c1f3a1aa0678d05e0d830fcff3078
l33th <l33th@protonmail.ch>
2022-01-27 06:11:44 +0200

    basic route setup and test

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("GoLang API")
+	router := mux.NewRouter()
+	router.HandleFunc("/test", test)
+	log.Fatalln(http.ListenAndServe(":8080", router))
+}
+
+func test(w http.ResponseWriter, _ *http.Request) {
+	log.Fatalln(w.Write([]byte("This is a test")))
 }

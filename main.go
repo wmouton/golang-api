4024364e8a41f33bd4734ba23291f335828b460f
l33th <l33th@protonmail.ch>
2022-01-28 15:34:18 +0200

    added comments

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	// Standard library imports
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	// Third party imports
 	"github.com/gorilla/mux"
 )
 
 // Post struct represents the post data
 type Post struct {
 	Title	string	`json:"title"`
-	Info	string	`json:"Info"`
+	Body	string	`json:"body"`
 	Author	*Author	`json:"author"`
 }
 
@@ -25,18 +27,27 @@ type Author struct {
 
 var posts []Post
 
+// Entry Point
 func main() {
+	// Indicates that the server is running on port 5000.
 	fmt.Println("server running on port 5000")
+	// Create a new mux router.
 	router := mux.NewRouter()
-	router.HandleFunc("/add", addItem).Methods("POST")
+	// Handler functions
+	router.HandleFunc("/add", addPost).Methods("POST")
+	// Listen and serve on port 5000 - log the errors
 	log.Fatalln(http.ListenAndServe(":5000", router))
 }
 
-func addItem(w http.ResponseWriter, r *http.Request) {
-	// get Item value from the JSON body
+// addPost adds a new post
+func addPost(w http.ResponseWriter, r *http.Request) {
+	// Get the Post value from the JSON body
 	var newPost Post
+	// Decode the JSON data
 	_ = json.NewDecoder(r.Body).Decode(&newPost)
+	// Append each new post to the posts array
 	posts = append(posts, newPost)
+	// Send data back as JSON in the header
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(posts); err != nil {
 		log.Fatalln(err, "error parsing json data")

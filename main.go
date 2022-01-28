0e284f1ceb2758a51e194314f34189a61bb5d3fc
l33th <l33th@protonmail.ch>
2022-01-28 10:58:41 +0200

    created custom type

--- main.go
+++ main.go
@@ -9,18 +9,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var data []string
+type Item struct {
+	data	string
+}
+
+var data []Item
 
 func main() {
 	fmt.Println("server running on port 5000")
 	router := mux.NewRouter()
-	router.HandleFunc("/add/{item}", addItem).Methods("POST")
+	router.HandleFunc("/add", addItem).Methods("POST")
 	log.Fatalln(http.ListenAndServe(":5000", router))
 }
 
 func addItem(w http.ResponseWriter, r *http.Request) {
-	routeVar := mux.Vars(r)["item"]
-	data = append(data, routeVar)
+	// get Item value from the JSON body
+	var newItem Item
+	if err := json.NewDecoder(r.Body).Decode(&newItem); err != nil {
+		log.Fatalln(err, "Error... Could not decode request body.")
+	}
+	data = append(data, newItem)
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatalln(err, "error parsing json data")
